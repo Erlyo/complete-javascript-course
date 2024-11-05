1e9df2e70fda11321ca2a47e2b9cc7c8a5049c60
Erlyo <erlendyork@gmail.com>
2024-11-05 21:02:07 +0100

    Studying JavaScript Fundamentals - Part 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+let country = "Norway";
+let continent = 'Europe';
+let population = 5000000;
+
+console.log(country);
+console.log(continent);
+console.log(population); 
+
+let isIsland = false;
+let language =
+
+    console.log
